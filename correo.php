156b88adaa1373b19ed8fe53bd113f2b8fc3aa8c
Ariel Edgardo Orellana <66234928+ArielOrellana@users.noreply.github.com>
2022-04-01 03:03:31 -0300

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,21 @@
+<?php 
+	// Datos para el correo
+	$destinatario = 'orellanaariel1998@gmail.com';
+	
+	$nombre = $_POST['nombre'];
+	$email = $_POST['email'];
+	$asunto = $_POST['asunto'];
+	$asunto = $_POST['mensaje'];
+
+
+	$header = "Contacto desde ";
+
+	$carta = `De: $nombre\n Correo: $email \n Mensaje $mensaje\n`;
+
+
+	// Enviando mensaje
+	mail($destinatario, $asunto, $carta, $header);
+	echo "<script>alert('correo enviado exitosamente')</script>";
+	echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+
+ ?>
