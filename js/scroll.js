1e135ea6bf31bf08fd387596de1bd244acd72bdc
Ariel Edgardo Orellana <66234928+ArielOrellana@users.noreply.github.com>
2022-04-16 03:04:44 -0300

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+    sr.reveal('.navbar', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+    sr.reveal('.nombre', {
+        duration: 2000,
+        rotate:{
+            x:1,
+            y:180,
+        }
+    });
+    sr.reveal('.perfil', {
+        duration: 3000,
+        origin: 'top',
+        
+    });
+    sr.reveal('.sobrei', {
+        duration: 3000,
+        origin: 'left',
+        distance: '100px'
+    });
+    sr.reveal('.sobreii', {
+        duration: 3000,
+        origin: 'rigth',
+        distance: '100px'
+    });
+    sr.reveal('.maill', {
+        duration: 2000,
+        origin: 'left',
+        distance: '300px'
+    });
